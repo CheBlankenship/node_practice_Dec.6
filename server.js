a3d708b3fea701c45033d3a8581f853ae525855b
Che B <cnakamura336@gmail.com>
2016-12-06 14:39:54 -0500

    update ser.js

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 
 app.get('/', function(req, res) {
   res.send('hello world');
 });
 
-app.get('/year', function(req, res) {
+app.post('/year', function(req, res) {
 
   var d = new Date();
   var year = d.getFullYear();
-  var age = req.body.age || '!';
+  console.log(year);
+  var age = req.body.age || 'years old';
 
-  res.send('Ur born in ' + year - age );
+  var born = year -age;
+
+  res.send('Ur born in ' + born + '!');
   // res.json({
   //   message: 'Ur ' + age + '!'
   // });
@@ -22,6 +27,3 @@ app.get('/year', function(req, res) {
 app.listen(3000, function(){
   console.log('Example app listening on part 3000');
 });
-
-
-app.use(bodyParser.json());
